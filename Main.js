ed57321795461394ddd0d636fc95286a3a8e6132
niwokzaay <niwokzaay@gmail.com>
2024-07-17 20:57:01 -0400

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,33 @@
+let menuElem1 = document.getElementById("Qone");
+let titleElem1 = document.querySelector(".title1");
+
+titleElem1.onclick = function() {
+    menuElem1.classList.toggle("open");
+};
+
+
+let menuElem2 = document.getElementById("Qtwo");
+let titleElem2 = document.querySelector(".title2");
+let iB = document.getElementById("iconB");
+
+
+titleElem2.onclick = function() {
+    menuElem2.classList.toggle("open");
+};
+
+
+let menuElem3 = document.getElementById("Qthree");
+let titleElem3 = document.querySelector(".title3");
+
+titleElem3.onclick = function() {
+    menuElem3.classList.toggle("open");
+};
+
+
+let menuElem4 = document.getElementById("Qfour");
+let titleElem4 = document.querySelector(".title4");
+
+titleElem4.onclick = function() {
+    menuElem4.classList.toggle("open");
+};
+
